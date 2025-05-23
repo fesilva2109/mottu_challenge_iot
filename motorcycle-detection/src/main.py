cb651f613ee4e0412048125f6311b549a07aead7
Felipe Silva Maciel <158374279+fesilva2109@users.noreply.github.com>
2025-05-23 15:29:15 -0300

    Update main.py

--- motorcycle-detection/src/main.py
+++ motorcycle-detection/src/main.py
@@ -6,7 +6,7 @@ def run_notebook(notebook_path):
     with open(notebook_path) as f:
         nb = nbformat.read(f, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep = ExecutePreprocessor(timeout=600, kernel_name='venv_python')
     ep.preprocess(nb, {'metadata': {'path': os.path.dirname(notebook_path)}})
 
     print(f'Notebook \"{notebook_path}\" executado com sucesso.')
